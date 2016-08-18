8c8a89b4a80da96c50d91298f82cb0aa9186a2b9
Vince Elizaga <vncelizaga@gmail.com>
2016-08-18 11:57:25 +0800

    removed disable-tap

--- www/js/app.js
+++ www/js/app.js
@@ -25,24 +25,6 @@ app.run(function($ionicPlatform) {
   Parse.serverURL = 'https://muse-rest-api.herokuapp.com/parse';
 });
 
-app.directive('disableTap', function($timeout) {
-  return {
-    link: function() {
-      $timeout(function() {
-        // Find google places div
-        _.findIndex(angular.element(document.querySelectorAll('.pac-container')), function(container) {
-          // disable ionic data tab
-          container.setAttribute('data-tap-disabled', 'true');
-          // leave input field if google-address-entry is selected
-          container.onclick = function() {
-            document.getElementById('autocomplete').blur();
-          };
-        });
-      },500);
-    }
-  };
-});
-
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
